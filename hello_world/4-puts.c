b6ff5a31d3524b89cdb93c6b2beb46f96c029b9d
serailcoder-io <omaranli56@gmail.com>
2025-06-24 09:16:24 +0000

    task 4: fix betty style warning

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+/**
+ * main - Entry point,
+ * print the text \"Programming is like building a multilingual puzzle
+ * and add a new line
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle\n");
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
 }
