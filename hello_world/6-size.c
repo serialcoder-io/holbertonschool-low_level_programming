ac43a6844be632f47e2327fc133fac17e313248e
serialcoder-io <142530504+serialcoder-io@users.noreply.github.com>
2025-06-24 18:39:59 +0400

    Update 6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -8,8 +8,8 @@ int main(void)
 {
 	printf("Size of a char: %d\n", sizeof(char));
 	printf("Size of a int: %d\n", sizeof(int));
-	printf("Size of a long int: %d\n", sizeof(long int));
-	printf("Size of a long long int: %d\n", sizeof(long long int));
-	printf("Size of a float: %d\n", sizeof(float));
+	printf("Size of a long int: %lu\n", sizeof(long int));
+	printf("Size of a long long int: %lu\n", sizeof(long long int));
+	printf("Size of a float: %lu\n", sizeof(float));
 	return (0);
 }
