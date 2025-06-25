15e5629268e92fb733f60a5b184196c82e61e5be
serialcoder-io <omaranli56@gmail.com>
2025-06-25 10:26:58 +0400

    feat: print the alphabet in lowercase, followed by a new line

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print alphabet followed by a new line
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch < 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
