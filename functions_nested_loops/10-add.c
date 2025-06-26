a879ce8dbb0d1cb25905863d80f0f4422468af2e
serialcoder-io <omaranli56@gmail.com>
2025-06-26 21:30:52 +0400

    task 10: added function that return the sum of the two given integer

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+/**
+ * add - add the 2 numbers passed in parametter
+ * @n1: first number
+ * @n2: second number
+ *
+ * Description: add n1 and n2
+ * and return the result
+ *
+ * Return: the sum of n1 and n2
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
