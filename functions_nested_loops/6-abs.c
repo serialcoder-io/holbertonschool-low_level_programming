1b0cb93a710d710bad16ef4bfa152304b904a0a3
serialcoder-io <omaranli56@gmail.com>
2025-06-26 14:30:54 +0400

    task 6: added a function that return the absolute value of the given number

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: the number to compute the absolute value of
+ *
+ * Description: Returns the absolute value of the integer passed
+ * as an argument.
+ *
+ * Return: the absolute value of n
+ */
+int _abs(int n)
+{
+	return (abs(n));
+}
