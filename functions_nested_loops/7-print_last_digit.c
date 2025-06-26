7a9b708a7d1d5ec88c93e091228ed74121eb2662
serialcoder-io <142530504+serialcoder-io@users.noreply.github.com>
2025-06-26 14:58:36 +0400

    Update 7-print_last_digit.c

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,17 +1,20 @@
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * print_last_digit - return last digit of given number
- * @n: the number we to return the last digit
+ * print_last_digit - prints the last digit of a number
+ * @n: the number to extract the last digit from
  *
- * Description: return the last digit of given number
- * eg print_digit(68) -> 8
+ * Description: Computes and prints the last digit of a number.
  *
- * Return: return the last digit of given number
+ * Return: The value of the last digit
  */
 int print_last_digit(int n)
 {
-	int last_digit = n % 10;
+	int last_digit;
+
+	last_digit = abs(n % 10);
+
+	_putchar(last_digit + '0');
 	return (last_digit);
 }
