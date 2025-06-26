65d8af803c65926da2947972a18d4899c095d2e5
serialcoder-io <omaranli56@gmail.com>
2025-06-26 11:59:10 +0400

    Task 1: extract the function print_alphabet from main

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,21 +1,6 @@
 #include "main.h"
 
 /**
- * main - Entry point
- *
- * Description: print the alphabet in lowercase followed by a new line
- * by calling print_alphabet function
- *
- * Return: Always 0 (success)
- */
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
-
-/**
  * print_alphabet - print the alphaet in lowercase
  *
  * Description: prinit alphabet in lowercase folled by a new line
