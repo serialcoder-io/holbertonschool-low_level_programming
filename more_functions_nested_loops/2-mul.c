59b38be857bf1ee74fff1e374fcf34a8d696199b
serialcoder-io <omaranli56@gmail.com>
2025-06-30 11:48:57 +0400

    task 2: created a function that takes to intergers as parameters and return the result of the multiplication

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+/**
+ * mul - Multiplies two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The result of the multiplication.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
