a8794ba710ecc697ee4112b287e063faabf536a2
serialcoder-io <omaranli56@gmail.com>
2025-06-30 13:57:49 +0400

    task 9: Add FizzBuzz program that prints numbers from 1 to 100 with substitutions

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the numbers from 1 to 100, but replaces
+ * multiples of 3 with "Fizz", multiples of 5 with "Buzz",
+ * and multiples of both with "FizzBuzz".
+ *
+ * Description: Each number or word is followed by a space.
+ * Uses the standard library and prints to standard output.
+ *
+ * Return: 0 (Success).
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3) == 0)
+		{
+			printf("Fizz ");
+		}
+		else if ((i % 5) == 0)
+		{
+			printf("Buzz ");
+		}
+		else if ((i % 3) == 0 && (i % 5) == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	return (0);
+}
