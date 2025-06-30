8db73823231515982ceab3283984d1ba689975d5
serialcoder-io <omaranli56@gmail.com>
2025-06-30 14:45:38 +0400

    task 10: Write a function that prints a triangle, followed by a new line.

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * print_triangle - Prints a triangle using the character #.
+ * @size: The size of the triangle.
+ *
+ * Description: Prints a right-aligned triangle where each row
+ * increases in width by one #. If size is 0 or less, only a
+ * newline is printed.
+ *
+ * Return: Nothing.
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 1; i < size; i++)
+		{
+			for (j = 1; j <= size; j++)
+			{
+				if (j <= (10 - i))
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar('#');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
